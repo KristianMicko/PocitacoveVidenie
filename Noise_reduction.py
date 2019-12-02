a94c2f0d9eef222e3365c51aa5d20fe87ecbfaa4
RomanHalushka <49348710+RomanHalushka@users.noreply.github.com>
2019-12-02 17:22:45 +0100

    update code noise reduction and g calculation

--- Noise_reduction.py
+++ Noise_reduction.py
@@ -1,24 +1,16 @@
-import numpy as np
-def gaussian_kernel(size, sigma=1):
-    #vypocet velkosti masky
+def gaussian_kernel(self, size, sigma=1):
     size = int(size) // 2
     x, y = np.mgrid[-size:size+1, -size:size+1]
-    #vypocet hodnot matice masky podla gaussovej distribucie
     normal = 1 / (2.0 * np.pi * sigma**2)
     g =  np.exp(-((x**2 + y**2) / (2.0*sigma**2))) * normal
     return g
 
-def convolution(img, kernel):
-    #vypocet velkosti obrazka
-    hi, wi= img.shape
-    #vypocet velkosti masky
+def convolution(self, img, kernel):
+    hi, wi= image.shape
     hk, wk = kernel.shape
-    #vytvorenie matice z nul podla velkosti matice povodneho obrazka
-    image_padded = np.zeros(shape=(hi + hk - 0, wi + wk - 0))
-    #"ponorenie" obrazka "mora" nul
-    image_padded[hk//2:-hk//2, wk//2:-wk//2] = img
-    #roznasobovanie pixelov obrazka a masky podla predpisu konvolucie
-    out = np.zeros(shape=img.shape)
+    image_padded = np.zeros(shape=(hi + hk - 1, wi + wk - 1))
+    image_padded[hk//2:-hk//2, wk//2:-wk//2] = image
+    out = np.zeros(shape=image.shape)
     for row in range(hi):
         for col in range(wi):
             for i in range(hk):
