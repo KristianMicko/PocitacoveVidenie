1645541db8cedbfa8c39278a33c579bd4fe90477
Kristián Mičko <kristian.micko@student.tuke.sk>
2019-12-02 18:16:34 +0100

    reverznaUprava

--- gradient_calculation.py
+++ gradient_calculation.py
@@ -1,11 +1,16 @@
-def sobel_filters(self, img):
+import numpy as np
+import Noise_reduction
+
+def sobel_filters(img):
+    #Definovanie matic sobelovej masky
     Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
     Ky = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
-
-    Ix = ndimage.filters.convolve(img, Kx)
-    Iy = ndimage.filters.convolve(img, Ky)
-
+    #Vypocet konvolucii pixelov obrazu s maskou	
+    Ix = Noise_reduction.convolution(img, Kx)
+    Iy = Noise_reduction.convolution(img, Ky)
+    #Vypocet gradientu jednotlivych suradnic
     G = np.hypot(Ix, Iy)
     G = G / G.max() * 255
+    #Vypocet smeru gradientu
     theta = np.arctan2(Iy, Ix)
     return (G, theta)
